b718dbe7bf9ffbee6fe821d9af8b352a1a29b5ab
xarabae <xara@arnold-aa.com>
2019-06-28 14:13:25 +0200

    database dump update

--- public/inc/eta_for_itt.sql
+++ public/inc/eta_for_itt.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 24. Jun 2019 um 16:31
--- Server-Version: 10.3.16-MariaDB
--- PHP-Version: 7.3.6
+-- Generation Time: Jun 28, 2019 at 02:12 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,36 +19,37 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `eta_for_itt`
+-- Database: `eta_for_itt`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `aufgabe`
+-- Table structure for table `aufgabe`
 --
 
 CREATE TABLE `aufgabe` (
   `ID` int(11) NOT NULL,
   `Nummer` varchar(10) NOT NULL,
-  `Prüfungs_ID` int(11) NOT NULL,
-  `Prüfungsteil_ID` int(11) NOT NULL,
+  `Pruefungs_ID` int(11) NOT NULL,
+  `Pruefungsteil_ID` int(11) NOT NULL,
   `Aufgabenart_ID` int(11) NOT NULL,
   `Thema_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `aufgabe`
+-- Dumping data for table `aufgabe`
 --
 
-INSERT INTO `aufgabe` (`ID`, `Nummer`, `Prüfungs_ID`, `Prüfungsteil_ID`, `Aufgabenart_ID`, `Thema_ID`) VALUES
-(1, '1. a)', 1, 1, 6, 1),
-(2, '2. b)', 1, 3, 2, 2);
+INSERT INTO `aufgabe` (`ID`, `Nummer`, `Pruefungs_ID`, `Pruefungsteil_ID`, `Aufgabenart_ID`, `Thema_ID`) VALUES
+(1, '1. a)', 1, 1, 6, 3),
+(2, '1. b)', 1, 1, 1, 3),
+(4, '1.', 1, 2, 6, 4);
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `aufgabenart`
+-- Table structure for table `aufgabenart`
 --
 
 CREATE TABLE `aufgabenart` (
@@ -57,7 +58,7 @@ CREATE TABLE `aufgabenart` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `aufgabenart`
+-- Dumping data for table `aufgabenart`
 --
 
 INSERT INTO `aufgabenart` (`ID`, `Bezeichnung`) VALUES
@@ -71,7 +72,7 @@ INSERT INTO `aufgabenart` (`ID`, `Bezeichnung`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `fach`
+-- Table structure for table `fach`
 --
 
 CREATE TABLE `fach` (
@@ -80,7 +81,7 @@ CREATE TABLE `fach` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `fach`
+-- Dumping data for table `fach`
 --
 
 INSERT INTO `fach` (`ID`, `Bezeichnung`) VALUES
@@ -92,7 +93,7 @@ INSERT INTO `fach` (`ID`, `Bezeichnung`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `pruefung`
+-- Table structure for table `pruefung`
 --
 
 CREATE TABLE `pruefung` (
@@ -102,21 +103,18 @@ CREATE TABLE `pruefung` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `pruefung`
+-- Dumping data for table `pruefung`
 --
 
 INSERT INTO `pruefung` (`ID`, `Jahr`, `Halbjahr`) VALUES
 (1, '2017', 'Sommer'),
 (2, '2016', 'Winter'),
-(3, '2016', 'Sommer'),
-(4, '2017', 'Sommer'),
-(5, '2016', 'Winter'),
-(6, '2016', 'Sommer');
+(3, '2016', 'Sommer');
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `pruefungsteil`
+-- Table structure for table `pruefungsteil`
 --
 
 CREATE TABLE `pruefungsteil` (
@@ -125,7 +123,7 @@ CREATE TABLE `pruefungsteil` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `pruefungsteil`
+-- Dumping data for table `pruefungsteil`
 --
 
 INSERT INTO `pruefungsteil` (`ID`, `Bezeichnung`) VALUES
@@ -136,7 +134,7 @@ INSERT INTO `pruefungsteil` (`ID`, `Bezeichnung`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `thema`
+-- Table structure for table `thema`
 --
 
 CREATE TABLE `thema` (
@@ -146,113 +144,115 @@ CREATE TABLE `thema` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `thema`
+-- Dumping data for table `thema`
 --
 
 INSERT INTO `thema` (`ID`, `Bezeichnung`, `Fach_ID`) VALUES
 (1, 'Datenbanken', 1),
-(2, 'Rechtsformen', 3);
+(2, 'Rechtsformen', 3),
+(3, 'UML', 1),
+(4, 'Arbeitsprozesse', 3);
 
 --
--- Indizes der exportierten Tabellen
+-- Indexes for dumped tables
 --
 
 --
--- Indizes für die Tabelle `aufgabe`
+-- Indexes for table `aufgabe`
 --
 ALTER TABLE `aufgabe`
   ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `Prüfungs_ID` (`Prüfungs_ID`,`Prüfungsteil_ID`,`Aufgabenart_ID`,`Thema_ID`),
+  ADD UNIQUE KEY `Prüfungs_ID` (`Pruefungs_ID`,`Pruefungsteil_ID`,`Aufgabenart_ID`,`Thema_ID`),
   ADD KEY `aufgabe_ibfk_1` (`Aufgabenart_ID`),
-  ADD KEY `aufgabe_ibfk_2` (`Prüfungsteil_ID`),
+  ADD KEY `aufgabe_ibfk_2` (`Pruefungsteil_ID`),
   ADD KEY `aufgabe_ibfk_4` (`Thema_ID`);
 
 --
--- Indizes für die Tabelle `aufgabenart`
+-- Indexes for table `aufgabenart`
 --
 ALTER TABLE `aufgabenart`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indizes für die Tabelle `fach`
+-- Indexes for table `fach`
 --
 ALTER TABLE `fach`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indizes für die Tabelle `pruefung`
+-- Indexes for table `pruefung`
 --
 ALTER TABLE `pruefung`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indizes für die Tabelle `pruefungsteil`
+-- Indexes for table `pruefungsteil`
 --
 ALTER TABLE `pruefungsteil`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indizes für die Tabelle `thema`
+-- Indexes for table `thema`
 --
 ALTER TABLE `thema`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `Fach_ID` (`Fach_ID`);
 
 --
--- AUTO_INCREMENT für exportierte Tabellen
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT für Tabelle `aufgabe`
+-- AUTO_INCREMENT for table `aufgabe`
 --
 ALTER TABLE `aufgabe`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT für Tabelle `aufgabenart`
+-- AUTO_INCREMENT for table `aufgabenart`
 --
 ALTER TABLE `aufgabenart`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT für Tabelle `fach`
+-- AUTO_INCREMENT for table `fach`
 --
 ALTER TABLE `fach`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT für Tabelle `pruefung`
+-- AUTO_INCREMENT for table `pruefung`
 --
 ALTER TABLE `pruefung`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT für Tabelle `pruefungsteil`
+-- AUTO_INCREMENT for table `pruefungsteil`
 --
 ALTER TABLE `pruefungsteil`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT für Tabelle `thema`
+-- AUTO_INCREMENT for table `thema`
 --
 ALTER TABLE `thema`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Constraints der exportierten Tabellen
+-- Constraints for dumped tables
 --
 
 --
--- Constraints der Tabelle `aufgabe`
+-- Constraints for table `aufgabe`
 --
 ALTER TABLE `aufgabe`
   ADD CONSTRAINT `aufgabe_ibfk_1` FOREIGN KEY (`Aufgabenart_ID`) REFERENCES `aufgabenart` (`ID`),
-  ADD CONSTRAINT `aufgabe_ibfk_2` FOREIGN KEY (`Prüfungsteil_ID`) REFERENCES `pruefungsteil` (`ID`),
-  ADD CONSTRAINT `aufgabe_ibfk_3` FOREIGN KEY (`Prüfungs_ID`) REFERENCES `pruefung` (`ID`),
+  ADD CONSTRAINT `aufgabe_ibfk_2` FOREIGN KEY (`Pruefungsteil_ID`) REFERENCES `pruefungsteil` (`ID`),
+  ADD CONSTRAINT `aufgabe_ibfk_3` FOREIGN KEY (`Pruefungs_ID`) REFERENCES `pruefung` (`ID`),
   ADD CONSTRAINT `aufgabe_ibfk_4` FOREIGN KEY (`Thema_ID`) REFERENCES `thema` (`ID`);
 
 --
--- Constraints der Tabelle `thema`
+-- Constraints for table `thema`
 --
 ALTER TABLE `thema`
   ADD CONSTRAINT `thema_ibfk_1` FOREIGN KEY (`Fach_ID`) REFERENCES `fach` (`ID`);
